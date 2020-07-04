ccd16a81557934b6d802837ee337fc978d5087c2
shinji <agongji27@gmail.com>
2020-07-05 02:31:38 +0900

    implemented a basic 4 arithmetic operations. Need to revisit.

--- 9cc.c
+++ 9cc.c
@@ -11,6 +11,7 @@ typedef enum {
 	TK_EOF,
 } TokenKind;
 
+/*Token type*/
 typedef struct Token Token;
 
 struct Token {
@@ -20,17 +21,21 @@ struct Token {
 	char * str;
 };
 
+
+/*input program*/
+char * user_input;
+
+// Current token
 Token * token;
 
-// void error(char * fmt, ...) {
-// 	va_list ap;
-// 	va_start(ap, fmt);
-// 	vfprintf(stderr, fmt, ap);
-// 	fprintf(stderr, "\n");
-// 	exit(1);
-// }
 
-char * user_input;
+void error(char * fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
 
 void error_at(char * loc, char * fmt, ...) {
 	va_list ap;
@@ -92,7 +97,7 @@ Token * tokenize() {
 			continue;
 		}
 
-		if (*p == '+' || *p == '-') {
+		if (strchr("+-*/()", *p)) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -103,7 +108,7 @@ Token * tokenize() {
 			continue;
 		}
 
-		error_at(p, "can not tokenize.");
+		error_at(p, "Invalid token.");
 	}
 
 	new_token(TK_EOF, cur, p);
@@ -111,34 +116,130 @@ Token * tokenize() {
 }
 
 
+/*paser*/
+typedef enum {
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+	NodeKind kind;
+	Node * lhs;
+	Node * rhs;
+	int val;
+};
+
+
+Node * new_node(NodeKind kind, Node * lhs, Node * rhs) {
+	Node * node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node * new_node_num(int val) {
+	Node * node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+}
+
+
+Node * expr();
+Node * mul();
+Node * primary();
+
+Node * expr() {
+	Node * node = mul();
+
+	for (;;) {
+		if (consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return(node);
+	}
+}
+
+Node * mul() {
+	Node * node = primary();
+
+	for (;;) {
+		if (consume('*'))
+			node = new_node(ND_MUL, node, primary());
+		else if (consume('/'))
+			node = new_node(ND_DIV, node, primary());
+		else
+			return(node);
+	}
+}
+
+Node * primary() {
+	if (consume('(')) {
+		Node * node = expr();
+		expect(')');
+		return(node);
+	}
+
+	return(new_node_num(expect_number()));
+}
+
+void gen(Node * node) {
+	if (node->kind == ND_NUM) {
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->kind) {
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("  sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("  imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("  cqo\n");
+		printf("  idiv rdi\n");
+		break;
+	}
+
+	printf("  push rax\n");
+}
 
 
 int main(int argc, char * argv[]){
-	if(argc != 2) {
-		fprintf(stderr, "argument numbers are incorrect.\n");
-		return(1);
-	}
+	if(argc != 2)
+		error("%s: Invalid number of arguments", argv[0]);
+	
 
 	user_input = argv[1];
 	token = tokenize();
+	Node * node = expr();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
 
+	gen(node);
 
-	printf("  mov rax, %d\n", expect_number());
 
-	while (!at_eof()){
-		if (consume('+')){
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("  sub rax, %d\n", expect_number());
-	}
-	
+	printf("  pop rax\n");
 	printf("  ret\n");
+
 	return(0);	
 }
